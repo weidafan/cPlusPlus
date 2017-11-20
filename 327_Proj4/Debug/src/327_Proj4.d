7e87abf77e9cf934b7c4f3f7c8289f2289adfae6
weida Fan <weida.fan.15@cnu.edu>
2017-11-19 23:38:47 -0500

    final

--- 327_Proj4/Debug/src/327_Proj4.d
+++ 327_Proj4/Debug/src/327_Proj4.d
@@ -1,10 +1,13 @@
 src/327_Proj4.o: ../src/327_Proj4.cpp \
  ../src/../../327_Proj4_Lib/includes/Smalltalk.h \
  ../src/../../327_Proj4_Lib/includes/Watch.h \
- ../src/../../327_Proj4_Lib/includes/Functions.h
+ ../src/../../327_Proj4_Lib/includes/Functions.h \
+ ../src/../../327_Proj4_Lib/includes/Smalltalk_Brit.h
 
 ../src/../../327_Proj4_Lib/includes/Smalltalk.h:
 
 ../src/../../327_Proj4_Lib/includes/Watch.h:
 
 ../src/../../327_Proj4_Lib/includes/Functions.h:
+
+../src/../../327_Proj4_Lib/includes/Smalltalk_Brit.h:
